6b69a0018ccc07373fee6093ac9bcdcd13a66220
Tiffany Lin <tifflin@uber.com>
2024-04-01 23:02:00 +0000

    Debug code coverage buildkite-agent permission issues

--- .buildkite/scripts/gocov.sh
+++ .buildkite/scripts/gocov.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
+echo "ls -la /usr/bin/"
+ls -la /usr/bin/
+whoami
+
 buildkite-agent artifact download ".build/*/coverage/unit_test_cover.out" . --step ":golang: unit-test" --build "$BUILDKITE_BUILD_ID"
 buildkite-agent artifact download ".build/*/coverage/integ_test_sticky_off_cover.out" . --step ":golang: integration-test-sticky-off" --build "$BUILDKITE_BUILD_ID"
 buildkite-agent artifact download ".build/*/coverage/integ_test_sticky_on_cover.out" . --step ":golang: integration-test-sticky-on" --build "$BUILDKITE_BUILD_ID"
